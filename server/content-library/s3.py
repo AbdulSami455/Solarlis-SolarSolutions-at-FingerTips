e0e975b533a30a380d6dff511ec09381b2ec741e
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-14 20:36:27 +0500

    content Library

--- server/content-library/s3.py
+++ server/content-library/s3.py
@@ -2,6 +2,8 @@ import boto3
 import logging
 import os
 
+
+#Listing all Buckets Present
 def list_bucket():
     # Create bucket
     try:
@@ -17,3 +19,29 @@ def list_bucket():
         return False
     return True
 list_bucket()
+
+def create_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except Exception as e:
+        logging.error(e)
+        return False
+    return True
+
+
+result_create = create_bucket("bs3s4s5s6s7")
+if result_create :
+    print("bucket got created successfully..!")
+else:
+    print("bucket got created failed..!")
+
+list_bucket()
+
+
