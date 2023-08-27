eae29408a4bec15861f188b9dfc51dd746b47438
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-27 21:58:36 +0500

    Adding Database

--- server/database.py
+++ server/database.py
@@ -4,16 +4,16 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
+
 load_dotenv()
 
-# Access environment variables
+
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-# Establish a connection to the database
+
 mydb = mysql.connector.connect(
     host=DB_HOST,
     user=DB_USER,
